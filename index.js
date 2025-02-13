a99413935ca8198473d39ea91908163f6633b699
colin <tresscd@gmail.com>
2025-02-13 13:51:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Server, Origins } = require("boardgame.io/server");
+const { TicTacToe } = require("./games/tictactoe.ts");
+
+const server = Server({
+  games: [TicTacToe],
+  origins: [Origins.LOCALHOST],
+});
+
+server.run(8000);
